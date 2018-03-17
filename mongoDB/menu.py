fe0dfd8f94936077fd9d65f482e445678a00d15b
Unknown <guschuma@student.gu.se>
2018-03-17 18:47:21 +0100

    added menu methods for listing and reading blogs

--- mongoDB/menu.py
+++ mongoDB/menu.py
@@ -18,7 +18,7 @@ class Menu(object):
         blog =  Database.find_one('blogs',{'author':self.user})
 
         if blog is not None:
-            self.user_blog = blog
+            self.user_blog = Blog.from_mongo(blog['id'])
             return True
         else:
             return False
@@ -34,11 +34,24 @@ class Menu(object):
     def run_menu(self):
        read_or_write = input("Do you want to read 'R' or write 'W' blogs")
        if read_or_write == 'R':
-
+           self._list_blogs()
+           self._view_blog()
            pass
        elif read_or_write == 'W':
-           pass
+           self.user_blog.new_post()
        else:
            print("Thank you gor blogging!")
 
 
+    def _list_blogs(self):
+        blogs = Database.find('blogs',{})
+
+        for blog in blogs:
+            print("ID: {}, Title: {}, Author: {}".format(blog['id'],blog['title'],blog['author']))
+
+    def _view_blog(self):
+        blog_to_see = input("Enter the ID of the blog you want to see")
+        blog = Blog.from_mongo(blog_to_see)
+        posts = blog.get_posts()
+        for post in posts:
+            print("Date: {}, Title: {}\n\n{}"/format(post['created_date'],post['title'],post['content']))
