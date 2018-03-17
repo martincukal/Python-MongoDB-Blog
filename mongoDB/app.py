58c7e3b0802a63e01673587e6ad7cd6b0fcddf02
Unknown <guschuma@student.gu.se>
2018-03-17 17:51:55 +0100

    added the blog.py

--- mongoDB/app.py
+++ mongoDB/app.py
@@ -3,28 +3,24 @@ __author__ = 'Martin Chukaleski '
 
 from database import Database
 from models.post import Post
+from models.blog import Blog
 
 
 
 Database.initialize()
 
-#post1 = Post(blog_id="123",title="Some other random post",content="some random content",author="Martin CH")
-#post1.save_in_mongo()
+blog = Blog(author="Martin",title="Sample title",description="Sample description")
 
-id = '82db07b779874834b8db9859c26483a5'
-#posts1 = Database.DATABASE['posts'].find_one({'id': id})
+blog.new_post()
 
-post1 = Post.from_mongo(id)
-post2 = Post.from_blog('123')
-print(post1)
-print('----------------------------')
-for post in post2:
-    print(post)
+blog.save_to_mongo()
+
+from_database = Blog.from_mongo(blog.id)
+
+print(blog.get_posts())
 
 
 
 
-#post = Post.from_mongo('e7067252fc7249c7b9e6a1f7c7b6be2c')
-#print(post)
 
 
